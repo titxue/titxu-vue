f12e699b2668ca627062a9f9e1cc9bd1826a7f30
lxue <xue283866@gmail.com>
2023-01-07 16:24:02 +0800

    :tada: oauth2.1+springboot3

---
+++ authorization-server-samples/src/main/java/com/titxu/oauth2/authorizationserversamples/AuthorizationServerSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.titxu.oauth2.authorizationserversamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerSamplesApplication.class, args);
+    }
+
+}
---
+++ authorization-server-samples/src/test/java/com/titxu/oauth2/authorizationserversamples/AuthorizationServerSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titxu.oauth2.authorizationserversamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
