0a455aad77af1c79a7934b4b14fbaac4c28b76a2
lxue <595242736@qq.com>
2023-01-14 21:18:48 +0800

    :goal_net: 全局捕获参数异常，修改为spring.imports进行全局异常配置类自动装配

--- titxu-common/titxu-common-web/src/main/java/com/titxu/cloud/common/web/constant/ResultCode.java
+++ titxu-common/titxu-common-web/src/main/java/com/titxu/cloud/common/web/constant/ResultCode.java
@@ -5,9 +5,6 @@ import lombok.NoArgsConstructor;
 
 /**
  * 返回数据定义
- *
-
-
  */
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,7 +23,12 @@ public enum ResultCode {
     /**
      * 禁止删除系统管理员
      */
-    DELETE_SYSTEM_ERROR(500, "禁止删除系统管理员"),
+    DELETE_SYSTEM_ERROR(2000, "禁止删除系统管理员"),
+
+    /**
+     * 参数错误
+     */
+    PARAM_ERROR(3000, "参数错误"),
 
     /**
      * 认证失败
@@ -36,6 +38,15 @@ public enum ResultCode {
     private int code;
 
     private String msg;
+
+    public static ResultCode getValue(int code) {
+        for (ResultCode value : values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        return null;
+    }
 
     public Integer getCode() {
         return code;
@@ -51,15 +62,5 @@ public enum ResultCode {
                 "\"code\":\"" + code + '\"' +
                 ", \"msg\":\"" + msg + '\"' +
                 '}';
-    }
-
-
-    public static ResultCode getValue(int code) {
-        for (ResultCode value : values()) {
-            if (value.getCode() == code) {
-                return value;
-            }
-        }
-        return null;
     }
 }
