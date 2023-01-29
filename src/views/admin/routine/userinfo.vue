14d9ddf0d3e12eb57d69d9cedbc06d8346fa9a07
lxue <595242736@qq.com>
2023-01-29 19:57:12 +0800

    :lipstick: 新建个人资料页面

---
+++ src/views/admin/routine/userinfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- Your content -->
+    <h1>个人资料</h1>
+  </div>
+</template>
+
+<script setup name="ViewsAdminRoutineUserinfo">
+  // const { proxy } = getCurrentInstance()
+  // const router = useRouter()
+  // const route = useRoute()
+</script>
+
+<style lang="less" scoped></style>
